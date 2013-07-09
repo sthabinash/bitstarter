759979fd3f60fee701cb535056adf29d146de818
unknown <abinash@dt52.yomaridomain.com>
2013-07-09 14:02:40 +0545

    web.js reads from index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var outFile = "index.html";
+    var fileBuffer=fs.readFileSync(outFile);
+    var line=fileBuffer.toString();
+    response.send(line);
 });
 
 var port = process.env.PORT || 5000;
